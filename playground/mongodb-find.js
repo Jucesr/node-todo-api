2f9cf94e9dd9dd9204da6e39793c4214c09ff53b
Julio Ojeda <jcom.94m@gmail.com>
2017-09-08 09:48:40 -0700

    Init repository

---
+++ playground/mongodb-find.js
@@ -0,0 +1,22 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB server');
+  }
+
+  console.log('Connected to MongoDB server');
+
+  db.collection('Users').find({
+    name: 'Julio'
+  }).toArray().then( (docs) =>{
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    if(err){
+      console.log('Unable to fetch user', err);
+    }
+  });
+
+  db.close();
+});
