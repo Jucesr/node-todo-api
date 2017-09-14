61cd7b671343eb7d95b5ee4ffa1133535b188d1f
Julio Ojeda <jcom.94m@gmail.com>
2017-09-14 16:53:13 -0700

    Preparing for authentification

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,35 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 var User = mongoose.model('User', {
   email: {
-    require: true,
+    required: true,
     trim: true,
     type: String,
-    minLengh: 1
-  }
+    minLengh: 1,
+    unique: true,
+    validate: {
+      validator: (value) =>{
+        return validator.isEmail(value);
+      },
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minLengh: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
 module.exports = {User};
