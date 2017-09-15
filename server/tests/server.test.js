578b0a441c77ba2f1fe3b9ed54b07c4e46f17ab8
Julio Ojeda <jcom.94m@gmail.com>
2017-09-15 15:21:42 -0700

    Add seeding data to test users.

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,28 +4,10 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
-const todos = [{
-  _id: new ObjectID(),
-  text: "First test todo"
-},{
-  _id: new ObjectID(),
-  text: "Second test todos",
-  completed: true,
-  completedAt: 333
-}];
-
-beforeEach( (done) =>{
-  Todo.remove({
-
-  }).then( () => {
-    return Todo.insertMany(todos);
-  }).then( () => {
-    done();
-  }).catch( (e) => {
-    console.log('There was an error ', e);
-  } );
-});
+beforeEach( populateUsers );
+beforeEach( populateTodos );
 
 describe( 'POST /todos', () => {
 
