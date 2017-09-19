12a5888bc0ff1260398c829456bf2cad51df0f39
Julio Ojeda <jcom.94m@gmail.com>
2017-09-18 18:03:33 -0700

    Add POST /users/login

--- server/server.js
+++ server/server.js
@@ -121,6 +121,26 @@ app.post('/users', (req, res) => {
 
 });
 
+app.post('/users/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+  var temp_user;
+
+  User.findByCredentials(body.email, body.password).then( (user) => {
+    temp_user = user;
+    return user.generateAuthToken()
+  }).then( (token) =>{
+    res.header('x-auth',token).send(temp_user);
+  }).catch( (e) => {
+    res.status(400).send();
+  });
+
+  // password = jwt.verify();
+  //
+  // User.findOne({body}).then( (user) =>{
+  //   res.status(200).send(user);
+  // });
+});
+
 app.get('/users/me',authenticate, (req, res) => {
   res.send(req.user);
 });
